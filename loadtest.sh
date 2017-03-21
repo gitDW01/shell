592c77645e70d44cb4ffa50b802c7d81f87fe73d
nick <maildw.01@gmail.com>
2017-03-21 10:43:28 +0300

    Initial commit

--- loadtest.sh
+++ loadtest.sh
@@ -3,7 +3,7 @@
 mkdir ./trash
 for ((i=1; i<=$1; i++))
 do
-(time (wget -P ./trash $2 > /dev/null 2>&1) 2>&1) | grep real » log.txt &
+(time (wget -P ./trash $2 > /dev/null 2>&1) 2>&1) | grep real >> log.txt &
 done
 wait
 rm -rf ./trash
