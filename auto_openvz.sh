0614c9cb124f118f8609d7467da6388aed865710
nick <maildw.01@gmail.com>
2017-03-31 18:31:28 +0300

    auto_openvz for Chef

---
+++ auto_openvz.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+case "$1" in
+ubuntu)
+knife bootstrap --run-list "recipe[proxmox::new_ubuntu_openvz]" hwser02.xbs.corp -P 12QWaszx > machine.txt
+cat machine.txt | grep machine: | awk -F: '{print $2}' > machine1.txt
+echo $(cat machine1.txt | tail -n 2) | tr -d '\r' >> /etc/hosts
+cat /etc/hosts | tail -n 1 | awk -F" " '{print $2}' > machine3.txt
+DESTINATION=$(cat machine3.txt)
+knife bootstrap --run-list "recipe[proxmox::hosts]" $DESTINATION -P 12QWaszx
+;;
+
+centos)
+knife bootstrap --run-list "recipe[proxmox::new_centos_openvz]" hwser02.xbs.corp -P 12QWaszx > machine.txt
+cat machine.txt | grep machine: | awk -F: '{print $2}' > machine1.txt
+echo $(cat machine1.txt | tail -n 2) | tr -d '\r' >> /etc/hosts
+cat /etc/hosts | tail -n 1 | awk -F" " '{print $2}' > machine3.txt
+DESTINATION=$(cat machine3.txt)
+knife bootstrap --run-list "recipe[proxmox::hosts]" $DESTINATION -P 12QWaszx
+;;
+
+esac
